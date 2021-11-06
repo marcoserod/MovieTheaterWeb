4e6f12edf2244723042a630222606a966c829c75
Marcos Rodriguez <mer.marcosrQgmail.com>
2021-11-06 14:25:13 -0300

    Add Skeleton for Discovery Loading

--- src/components/pages/Discovery/index.jsx
+++ src/components/pages/Discovery/index.jsx
@@ -1,4 +1,5 @@
-import { Card, Container, Grid } from "@mui/material";
+import { Card, Container, Grid, Skeleton } from "@mui/material";
+import { maxHeight } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { getMoviesByDiscovery } from "../../../services/discover";
 import MovieCard from "../../molecules/MovieCard";
@@ -6,6 +7,7 @@ import { PageTemplate } from "../../templates";
 import { Wrapper } from "./styles";
 
 export const Discovery = () => {
+  const [loading, setLoading] = useState(true);
   const [moviesData, setMoviesData] = useState(null);
 
   const handleGetMovies = async () => {
@@ -14,6 +16,7 @@ export const Discovery = () => {
       const { results } = response;
       setMoviesData(results);
     } catch (error) {}
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -23,17 +26,32 @@ export const Discovery = () => {
   return (
     <PageTemplate>
       <Wrapper>
-        {moviesData
-          ? moviesData.map((movie) => (
-              <Grid key={movie.id} item>
-                <MovieCard
-                  key={movie.id}
-                  title={movie.title}
-                  img={movie.backdrop_path}
-                />
-              </Grid>
-            ))
-          : null}
+        {loading &&
+          [...Array(9).keys()].map((index) => (
+            <div key={index} style={{ maxHeight: "370px" }}>
+              <Skeleton
+                sx={{ height: 190 }}
+                animation="wave"
+                variant="rectangular"
+              />
+              <Skeleton
+                animation="wave"
+                height={10}
+                style={{ marginBottom: 6 }}
+              />
+              <Skeleton animation="wave" height={10} width="80%" />
+            </div>
+          ))}
+        {moviesData &&
+          moviesData.map((movie) => (
+            <Grid key={movie.id} item>
+              <MovieCard
+                key={movie.id}
+                title={movie.title}
+                img={movie.backdrop_path}
+              />
+            </Grid>
+          ))}
       </Wrapper>
     </PageTemplate>
   );
